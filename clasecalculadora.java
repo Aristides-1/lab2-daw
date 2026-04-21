40223a8d40fb71c2c8f86da835b56a0cf276fcd4
Aristides-1 <dcervantesapa@unsa.edu.pe>
2026-04-21 09:42:47 -0500

    Add files via upload

---
+++ clasecalculadora.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Calculator {
+
+    int add(int a, int b){ 
+        
+        
+        
+        
+        
+        return 0; }
+
+
+        
+    int sub(int a, int b){ 
+        
+        
+        return 0; }
+    int mul(int a, int b){ 
+        
+        
+        
+        
+        
+        return 0; }
+    int div(int a, int b){ 
+        
+        
+        return 0; }
+    int mod(int a, int b){ 
+        
+        
+        
+        
+        return 0; }
+    }
